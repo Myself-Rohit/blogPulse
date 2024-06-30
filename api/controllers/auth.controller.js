5bfc8d1b0a78486396dc4620d7da82d766771262
rohit negi <rohitnegi263139@gmail.com>
2024-06-30 22:06:10 +0530

    create middleware and signup page interface

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import user from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
-    return res.status(400).json({ message: "All fields are reqired" });
+    return next(errorHandler(400, "All fields are reqired"));
   }
 
   const hashPassword = bcryptjs.hashSync(password, 10);
@@ -17,6 +18,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.send("user created successfully");
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(error);
   }
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,56 @@
+import { Label, TextInput, Button } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex flex-col md:flex-row md:items-center mx-au5to max-w-3xl gap-5 p-3">
+        <div className="flex-1">
+          <Link to={"/"} className=" text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">
+              Rohit's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign up with your email and password
+            or with Google
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username"></Label>
+              <TextInput type="text" id="username" placeholder="username" />
+            </div>
+            <div>
+              <Label value="Your email"></Label>
+              <TextInput
+                type="email"
+                id="email"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div>
+              <Label value="Your password"></Label>
+              <TextInput type="password" id="password" placeholder="password" />
+            </div>
+
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/signin" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
